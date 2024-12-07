2ef43251adfe013ac7c552e8a2c1ffa9d4a1ad06
Desmond <adabedesmond@gmail.com>
2024-12-06 18:38:55 -0800

    modified

--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -3,7 +3,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(500).send(err)
+  return res.status(500).send('Something went wrong')
 }
 
 module.exports = errorHandlerMiddleware
