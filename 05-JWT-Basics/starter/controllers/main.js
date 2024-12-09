f10cc92d38bac27106453a10b80e9b54e62c5645
Desmond <adabedesmond@gmail.com>
2024-12-09 09:09:34 -0800

    modified:

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -7,27 +7,21 @@
 // require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const {BadRequestError} = require('../errors')
+const {StatusCodes} = require('http-status-codes');
 const login = async (req, res, next) => {
-    const { username, password } = req.body;
+   const {username, password} = req.body;
 
-    // mongo validation,
-    // joi,
-    // check in  the controller
+   if(!username ||!password){
+    throw new BadRequestError("Please provide username or password")
+   }
 
-    if (!username || !password) {
-        throw new BadRequestError('Please provide email and password');
-    }
+   const id = new Date().getDate()
+   const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
 
-    const id = new Date().getDate()
-
-    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
-
-    console.log(process.env.JWT_SECRET)
-
-    res.status(200).json({
-        msg: "User created",
-        token
-    })
+   res.status(StatusCodes.OK).json({
+     msg: 'User created',
+     token
+   })
 }
 
 const dashboard = async (req, res) => {
